27c3304d7342eddb5fb9f67d2f3af7d08145446f
andynesse <ann.nesse@stud.uis.no>
2026-03-22 19:50:43 +0100

    test: Add simple .../games/ enpoint for testing

--- boardgame_cafe/src/presentation/api/games.py
+++ boardgame_cafe/src/presentation/api/games.py
@@ -3,3 +3,11 @@ from flask import Blueprint
 bp = Blueprint("games", __name__, url_prefix="/games")
 
 # TODO: Add games routes here
+
+@bp.route("/", methods=["GET"])
+def list_games():
+    return {"message": "List of games will be here"}, 200
+
+@bp.route("/<int:game_id>", methods=["GET"])
+def get_game(game_id):
+    return {"message": f"Details for game {game_id} will be here"}, 200
