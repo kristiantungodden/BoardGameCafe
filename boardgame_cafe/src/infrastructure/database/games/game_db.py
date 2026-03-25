c4ccc7f4b468839e6e9a763e7eea6e7063c43460
kristiantungodden <kristian.tu@outlook.com>
2026-03-25 10:12:40 +0100

    database filstruktur

--- boardgame_cafe/src/infrastructure/database/games/game_db.py
+++ boardgame_cafe/src/infrastructure/database/games/game_db.py
@@ -14,16 +14,3 @@ class Game(db.Model):
     image_url = db.Column(db.Text)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
-
-class GameCopy(db.Model):
-    __tablename__ = "game_copies"
-
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable=False)
-    copy_code = db.Column(db.String(50), unique=True, nullable=False)
-    condition_note = db.Column(db.Text)
-    status = db.Column(db.String(20), nullable=False)
-    location = db.Column(db.String(50), nullable=False)
-    updated_at = db.Column(db.DateTime, server_default=db.func.now())
-
-    game = db.relationship("Game", backref="copies")
