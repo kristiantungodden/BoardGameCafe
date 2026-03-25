c06a22558df69a6d68c380df4c4764b99367013b
kristiantungodden <kristian.tu@outlook.com>
2026-03-25 10:42:44 +0100

    error i ref

--- boardgame_cafe/src/infrastructure/database/games/game_tag_db.py
+++ boardgame_cafe/src/infrastructure/database/games/game_tag_db.py
@@ -5,4 +5,9 @@ class GameTag(db.Model):
     __tablename__ = "game_tags"
 
     id = db.Column(db.Integer, primary_key=True)
+<<<<<<< HEAD
     name = db.Column(db.String(50), unique=True, nullable=False)
+=======
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+>>>>>>> 6208c66 (error i ref)
