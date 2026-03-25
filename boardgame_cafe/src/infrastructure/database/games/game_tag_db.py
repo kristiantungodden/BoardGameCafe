6208c66a3bd7ee187f9e611468c36da32dda5a74
kristiantungodden <kristian.tu@outlook.com>
2026-03-25 10:41:12 +0100

    error i ref

--- boardgame_cafe/src/infrastructure/database/games/game_tag_db.py
+++ boardgame_cafe/src/infrastructure/database/games/game_tag_db.py
@@ -7,13 +7,3 @@ class GameTag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
 
-
-class GameTagLink(db.Model):
-    __tablename__ = "game_tag_links"
-
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable=False)
-    game_tag_id = db.Column(db.Integer, db.ForeignKey("game_tags.id"), nullable=False)
-
-    game = db.relationship("Game", backref="tags")
-    tag = db.relationship("GameTag")
